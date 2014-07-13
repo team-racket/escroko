c550fd5b57fd7fbfe6478fd7d2fbb5c5f6248e93
Marc Plano-Lesay <marc.planolesay@gmail.com>
2014-07-13 14:50:11 +0200

    Work on flashes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def bootstrap_class_for flash_type
+    case flash_type
+    when 'success' then 'alert-success'
+    when 'error' then 'alert-error'
+    when 'alert' then 'alert-warning'
+    when 'notice' then 'alert-info'
+    else flash_type
+    end
+  end
 end
